0e577b6fdc6c2a1032c07c85a931d272c617b211
aker <kzjmail@gmail.com>
2018-01-31 07:50:30 +0800

    build myself js-frontend

---
+++ myjs-frontend/src/App2.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './App';
+import reducer from './reducer';
+
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/topics">Topics</Link></li>
+        <li><Link to="/hello">Hello</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/topics" component={Topics}/>
+      <Route path="/hello" component={Hello}/>
+    </div>
+  </Router>
+)
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+)
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
+
+// Store
+const store = createStore(reducer);
+
+const Hello = () => (
+  <div>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </div>
+)
+
+export default BasicExample
---
+++ myjs-frontend/src/client.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BasicExample from './App2';
+
+ReactDOM.render(
+  <BasicExample/>,
+  document.getElementById('app')
+);
