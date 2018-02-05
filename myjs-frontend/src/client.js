42678377ab9c6d0d402ac0b10f5ef46bf0dfddc1
aker <kzjmail@gmail.com>
2018-02-06 07:58:52 +0800

    build myself js-frontend

--- myjs-frontend/src/client.js
+++ myjs-frontend/src/client.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Router, Route, hashHistory } from 'react-router';
+
 import MyApp from './index';
 
 ReactDOM.render(
-  <MyApp />,
+  <Router history={hashHistory}>
+    <Route path="/" component={MyApp}/>
+  </Router>,
   document.getElementById('root')
 );
--- myjs-frontend/src/index.js
+++ myjs-frontend/src/index.js
@@ -3,7 +3,6 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducer';
 import App from './App';
-import { Router, Route, hashHistory } from 'react-router';
 
 const store = createStore(reducer);
 
